94d72164b11c8ff204a01bb711c704b96e106bf1
xieguigang <xie.guigang@gcmodeller.org>
2021-10-30 18:35:08 +0800

    merge source file

--- R/BISCUIT_main.R
+++ R/BISCUIT_main.R
@@ -34,49 +34,61 @@ singlecell_processor = function(
     alpha                    = 1,
     output_folder_name       = "./output") {
 
+    col_palette = color_palette(); 
+
+    ###output directory creation
+
+    if( dir.exists(paste0(getwd(),"/", output_folder_name))){
+        file.rename(paste0(getwd(),"/", output_folder_name),paste0(getwd(),"/","BISCUIT_previous_run","/"))
+    }
+
+    if(! dir.exists(paste0(getwd(),"/",output_folder_name))){
+        dir.create(paste0(getwd(),"/",output_folder_name,"/"))
+        dir.create(paste0(getwd(),"/",output_folder_name,"/plots/"))
+        dir.create(paste0(getwd(),"/",output_folder_name,"/plots/Inferred_labels/"))
+        dir.create(paste0(getwd(),"/",output_folder_name,"/plots/Inferred_labels_per_step_per_batch/"))
+        dir.create(paste0(getwd(),"/",output_folder_name,"/plots/Inferred_alphas_betas/"))
+        dir.create(paste0(getwd(),"/",output_folder_name,"/plots/Inferred_Sigmas/"))
+        dir.create(paste0(getwd(),"/",output_folder_name,"/plots/Inferred_means/"))
+        dir.create(paste0(getwd(),"/",output_folder_name,"/plots/extras/"))
+    }
+
+    ############## Run BISCUIT ##############
+
+    start_time_overall <- Sys.time()
+
+    #1) Prepare the input data. Explain what is input and what has to be the output.
+    process_data();
+
+    #2) Main MCMC engine. Do not change anything. This runs in parallel where each parallel run takes in a matrix X of all cells and a gene batch i.e. dim(X) is numcells x gene_batch.
+    IMM_Gibbs_MCMC_parallel();
+
+    #3) Postprocess MCMC chains from multiple parallel runs
+    post_MCMC_genesplit_merge();
+
+    #4) Compute imputed data based on inferred variables and generate plots
+    post_process();
+    ########################################
+
+    #print(Sys.time() - start_time_overall)
+    curr_time <- Sys.time()
+    print(curr_time - start_time_overall)
+    write(paste('Overall run time: ',curr_time - start_time_overall),file=f1, append=TRUE)
 }
 
-col_palette = color_palette(); 
+post_process = function() {
+    if(num_gene_batches ==1){
+        
+        source("BISCUIT_parallel_impute_onegenebatch.R")
+        source("BISCUIT_extras_onegenebatch.R")
+    
+    }else{
+        
+        source("BISCUIT_parallel_impute.R")
+        source("BISCUIT_extras.R")
 
-###output directory creation
-
-if( dir.exists(paste0(getwd(),"/", output_folder_name))){
-    file.rename(paste0(getwd(),"/", output_folder_name),paste0(getwd(),"/","BISCUIT_previous_run","/"))
+    }
 }
-
-if(! dir.exists(paste0(getwd(),"/",output_folder_name))){
-     dir.create(paste0(getwd(),"/",output_folder_name,"/"))
-     dir.create(paste0(getwd(),"/",output_folder_name,"/plots/"))
-     dir.create(paste0(getwd(),"/",output_folder_name,"/plots/Inferred_labels/"))
-     dir.create(paste0(getwd(),"/",output_folder_name,"/plots/Inferred_labels_per_step_per_batch/"))
-     dir.create(paste0(getwd(),"/",output_folder_name,"/plots/Inferred_alphas_betas/"))
-     dir.create(paste0(getwd(),"/",output_folder_name,"/plots/Inferred_Sigmas/"))
-     dir.create(paste0(getwd(),"/",output_folder_name,"/plots/Inferred_means/"))
-     dir.create(paste0(getwd(),"/",output_folder_name,"/plots/extras/"))
-}
-
-############## Run BISCUIT ##############
-
-start_time_overall <- Sys.time()
-
-#1) Prepare the input data. Explain what is input and what has to be the output.
-source("BISCUIT_process_data.R");
-
-
-#2) Main MCMC engine. Do not change anything. This runs in parallel where each parallel run takes in a matrix X of all cells and a gene batch i.e. dim(X) is numcells x gene_batch.
-source("BISCUIT_IMM_Gibbs_MCMC_parallel.R")
-
-#3) Postprocess MCMC chains from multiple parallel runs
-source("BISCUIT_post_MCMC_genesplit_merge.R")
-
-#4) Compute imputed data based on inferred variables and generate plots
-source("BISCUIT_post_process.R")
-########################################
-
-#print(Sys.time() - start_time_overall)
-curr_time <- Sys.time()
-print(curr_time - start_time_overall)
-write(paste('Overall run time: ',curr_time - start_time_overall),file=f1, append=TRUE)
 
 
 
